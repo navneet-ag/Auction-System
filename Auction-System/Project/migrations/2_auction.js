939de53e2ea001b4be2c6ad5c634852d6971aad0
falcon1422 <67194429+falcon1422@users.noreply.github.com>
2021-12-14 12:09:40 +0530

    Useless files removed

--- Auction-System/Project/migrations/2_auction.js
+++
@@ -1,5 +0,0 @@
-const auction_test = artifacts.require("Auction");
-
-module.exports = function (deployer) {
-  deployer.deploy(auction_test);
-};
--- Auction-System/Project/migrations/2_hello_world_migration.js
+++
@@ -1,5 +0,0 @@
-const Hello_world = artifacts.require("hello_world");
-
-module.exports = function (deployer) {
-  deployer.deploy(Hello_world);
-};
